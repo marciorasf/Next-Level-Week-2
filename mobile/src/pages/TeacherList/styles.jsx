9c8d5ca3259f6e66522ca7183627e4b4e9ceac09
Marcio Souza Filho <marciorasf@gmail.com>
2020-08-06 17:36:46 -0300

    add study pages

---
+++ mobile/src/pages/TeacherList/styles.jsx
@@ -0,0 +1,10 @@
+const { StyleSheet } = require("react-native");
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f7"
+  }
+})
+
+export default styles
